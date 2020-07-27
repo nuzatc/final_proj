354cd07f03253bc615e98ee16877c157319ba18f
Nuzat <nuzatchoudhury10@gmail.com>
2020-07-27 18:39:57 +0000

    last file

--- app.py
+++ app.py
@@ -3,44 +3,94 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_pymongo import PyMongo
+import os 
+
 # from flask_pymongo import PyMongo
-
-
 # -- Initialization section --
 app = Flask(__name__)
-
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
-
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
-
+app.config['MONGO_DBNAME'] = 'final_proj'
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
-
-# mongo = PyMongo(app)
-
+user = os.environ["user"]
+pw = os.environ["pw"]
+app.config['MONGO_URI'] = f'mongodb+srv://{user}:{pw}@cluster0.vuqlg.mongodb.net/final_proj?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+hor = mongo.db.hor
+senate = mongo.db.senate
+gov = mongo.db.gov
+mayor = mongo.db.mayor
 # -- Routes section --
 # INDEX
-
+ # --- HOMEPAGE WITH A FORM TO SUBMIT A ZIPCODE
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return render_template('index.html', events = events)
+    #step1 connect to database
+    hor1 = mongo.db.hor
+    senate1 = mongo.db.senate
+    gov1 = mongo.db.gov
+    #step2 do a query for all events in db
+    hors = list(hor1.find({}))
+    senates = list(senate1.find({}))
+    govs = list(gov1.find({}))
+    print(hors)
+    print(senates)
+    print(govs)
 
+    #step3 return render template
+    return render_template('index.html', hors = hors, senates = senates, govs = govs)
 
+@app.route('/yourRepresentatives', methods = ["GET", "POST"])
+def yourRepresentatives():
+    if request.method == "POST":
+        #RENDER TEMPLATE 
+        #1 get data from form
+        user_district = request.form["district"]
+        #2 print data
+        print(user_district)
+        #2.5 connect database 
+        hor1 = mongo.db.hor
+        senate1 = mongo.db.senate
+        gov1 = mongo.db.gov
+        #3 query mongo zipcodes that match user input
+        horss = list(hor1.find({"district": user_district}))[0]
+        senatess = list(senate1.find({}))[0]
+        senatesss = list(senate1.find({}))[1]
+        govss = list(gov1.find({}))[0]
+        print(horss)
+        print(senatess)
+        print(govss)
+        #4 print mongo query
+        return render_template('yourRepresentatives.html', horss = horss, senatess = senatess, govss = govss, senatesss = senatesss)
+    else:
+        return "hello hello"
+
+@app.route('/YourRepresentatives/<collection>/<politician>' , methods = ["GET", 'POST'])
+def politicianPage(collection, politician):
+    # user_district = request.form["district"]
+    if collection == "hor":
+        poli1 = mongo.db.hor
+        poli = list(poli1.find({"name": politician}))[0]
+        print(poli)
+        return render_template('SpecificRepresentative.html', poli = poli)
+    elif collection == "senate":
+        poli1 = mongo.db.senate
+        poli = list(poli1.find({"name": politician}))[0]
+        print(poli)
+        return render_template('SpecificRepresentative.html', poli = poli)
+    elif collection == "gov":
+        poli1 = mongo.db.gov
+        poli = list(poli1.find({"name": politician}))[0]
+        print(poli)
+        return render_template('SpecificRepresentative.html', poli = poli)
 # CONNECT TO DB, ADD DATA
+# @app.route('/add')
+# def add():
+#     # connect to the database
+#     # insert new data
+#     # return a message to the user
+#     return ""
 
-@app.route('/add')
 
-def add():
-    # connect to the database
 
-    # insert new data
-
-    # return a message to the user
-    return ""
